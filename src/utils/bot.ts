8671c7d60ddc151f247b525834a1819895dcc667
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-06-21 22:51:28 -0400

    Update bot.ts

--- src/utils/bot.ts
+++ src/utils/bot.ts
@@ -1,6 +1,6 @@
 import { Context } from "hono";
 
-const BOT_REGEX = /bot|facebook|embed|got|firefox\/92|firefox\/38|curl|wget|go-http|yahoo|generator|whatsapp|revoltchat|preview|link|proxy|vkshare|images|analyzer|index|crawl|spider|python|cfnetwork|node|mastodon|http\.rb|ruby|bun\/|fiddler|iframely|steamchaturllookup|discordbot|telegrambot|slackbot|twitterbot|applebot|googlebot|january|synapse/i;
+const BOT_REGEX = /bot|facebook|embed|got|firefox\/\d+|curl|wget|go-http|yahoo|generator|whatsapp|revoltchat|preview|link|proxy|vkshare|images|analyzer|index|crawl|spider|python|cfnetwork|node|mastodon|http\.rb|ruby|bun\/|fiddler|iframely|steamchaturllookup|discordbot|telegrambot|slackbot|twitterbot|applebot|googlebot|january|synapse/i;
 
 export function isBot(userAgent: string | undefined | null): boolean {
   if (!userAgent) return false;
